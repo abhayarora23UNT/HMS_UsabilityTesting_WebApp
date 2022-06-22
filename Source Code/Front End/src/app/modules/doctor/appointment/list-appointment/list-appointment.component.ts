4ab874485b89c3180351b8475170d89d171c33d5
jaidev Gilla <jaidevgilla@jaidevs-MacBook-Air.local>
2022-06-21 22:45:24 -0500

    changes to add hospital,doctor,patient names in Appointment tab

--- Source Code/Front End/src/app/modules/doctor/appointment/list-appointment/list-appointment.component.ts
+++ Source Code/Front End/src/app/modules/doctor/appointment/list-appointment/list-appointment.component.ts
@@ -4,6 +4,7 @@ import { NavigationExtras, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { Messages } from 'src/app/core/messages/messages';
 import { DoctorAppointmentService } from 'src/app/core/services/doctor/doctor-apppointment.service';
+import { LookupService } from 'src/app/core/services/lookups/lookups.service';
 import { ToastMessageService } from 'src/app/core/services/utils/toast-message.service';
 import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
 import { DoctorAppointment } from 'src/app/shared/models/doctor/doctor-appointment-resp-data';
@@ -15,12 +16,15 @@ import { DoctorAppointment } from 'src/app/shared/models/doctor/doctor-appointme
 })
 export class ListAppointmentComponent implements OnInit, OnDestroy {
 
+  hospitalIdList: any = []; // dropdown list for hospital Id
+  doctorIdList: any = [];   // dropdown list for doctor Id
+  patientIdList: any = [];  // dropdown list for patient Id
 
-  appointmentColumns: string[] = ['doctorId', 'patientId', 'appt_Date', 'fee', 'action'];  // table columns
+  appointmentColumns: string[] = ['doctorId', 'doctorName','patientId','patientName','hospitalName', 'appt_Date', 'fee', 'action'];  // table columns
   isDataLoading = false; // flag to hide/show loader
   private onDestroy$: Subject<void> = new Subject<void>();
   dataSource: any = [];
-  constructor(private doctorService: DoctorAppointmentService, private toastService: ToastMessageService, private router: Router, private dialog: MatDialog) {
+  constructor(private doctorService: DoctorAppointmentService,private lookupService: LookupService, private toastService: ToastMessageService, private router: Router, private dialog: MatDialog) {
 
   }
 
@@ -28,7 +32,14 @@ export class ListAppointmentComponent implements OnInit, OnDestroy {
    * Method called on page init
    */
   ngOnInit(): void {
-    this.getDocAppointmentList();
+    this.getHospitalBranchList();
+    this.getDoctorsList();
+    this.getPatientsList();
+
+    setTimeout(() => {
+      this.getDocAppointmentList();
+    }, 1000);
+    
   }
 
   /**
@@ -67,6 +78,63 @@ export class ListAppointmentComponent implements OnInit, OnDestroy {
       });
   }
 
+  getHospitalBranchList() {
+    this.lookupService.getHospitalBranchList()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((retData: any) => {
+        if (retData.status) {
+          if (retData.data.Table != null && retData.data.Table.length > 0) {
+            this.hospitalIdList = retData.data.Table;
+          }
+          else {
+            this.toastService.infoMessage(Messages.No_Records_Message);
+          }
+        } else {
+          this.toastService.errorMessage(retData.message);
+        }
+      });
+  }
+
+  /**
+   * Function called to get doctors List
+   */
+  getDoctorsList() {
+    this.lookupService.getDoctorsList()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((retData: any) => {
+        if (retData.status) {
+          if (retData.data.Table != null && retData.data.Table.length > 0) {
+            this.doctorIdList = retData.data.Table;
+          }
+          else {
+            this.toastService.infoMessage(Messages.No_Records_Message);
+          }
+        } else {
+          this.toastService.errorMessage(retData.message);
+        }
+      });
+  }
+
+  /**
+   * Function called to get patient List
+   */
+  getPatientsList() {
+    this.lookupService.getPatientsList()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((retData: any) => {
+        if (retData.status) {
+          if (retData.data.Table != null && retData.data.Table.length > 0) {
+            this.patientIdList = retData.data.Table;
+          }
+          else {
+            this.toastService.infoMessage(Messages.No_Records_Message);
+          }
+        } else {
+          this.toastService.errorMessage(retData.message);
+        }
+      });
+  }
+
   /**
    * Method to parse appointment list response
    * @param retData 
@@ -79,6 +147,9 @@ export class ListAppointmentComponent implements OnInit, OnDestroy {
       respObj.appointmentId = row.AppointmentId;
       respObj.appt_Date = row.Appt_Date;
       respObj.doctorId = row.DoctorId;
+      respObj.doctorName = this.getDoctorName(row.DoctorId);
+      respObj.patientName = this.getPatientName(row.PatientId);
+      respObj.hospitalName = this.getHospitalName(row.HospitalId);
       respObj.fee = row.Fee;
       respObj.diseaseNotes = row.DiseaseNotes;
       respObj.hospitalId = row.HospitalId;
@@ -134,6 +205,42 @@ export class ListAppointmentComponent implements OnInit, OnDestroy {
       }
     });
 
+  }
+
+  /**
+    * Method to get doctor name from id
+    * @param doctorId 
+    */
+  getDoctorName(doctorId:any){
+    for(const row of this.doctorIdList){
+      if(row.DoctorId == doctorId)
+      return row.Name;
+    }
+    return "";
+  }
+
+  /**
+    * Method to get patient name from id
+    * @param patientId 
+    */
+  getPatientName(patientId:any){
+    for(const row of this.patientIdList){
+      if(row.PatientId == patientId)
+      return row.Name;
+    }
+    return "";
+  }
+
+  /**
+    * Method to get hospital name from id
+    * @param hospitalId 
+    */
+  getHospitalName(hospitalId:any){
+    for(const row of this.hospitalIdList){
+      if(row.HospitalId == hospitalId)
+      return row.Name;
+    }
+    return "";
   }
 
   /**
